779c6ef55ecee23654170bb959e24c42ac911465
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-04-12 12:46:17 +0200

    Added logos and improved design

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,29 +8,39 @@ export const MovieView = ({ movies }) => {
     const movie = movies.find((m) => m.id === movieId);
 
     return (
-        <div>
+        <div className="movie-container">
             <div>
-                <img src={movie.image} />
+                <img
+                    className="movie-image"
+                    src={movie.image}
+                    alt="movie image"
+                />
             </div>
-            <div>
-                <span>Title:</span>
-                <span>{movie.title}</span>
+            <div className="movie-details">
+                <div className="movie-info">
+                    <span className="info-label">Title: </span>
+                    <span className="info-text">{movie.title || "No Title"}</span>
+                </div>
+                <div className="movie-info">
+                    <span className="info-label">Description: </span>
+                    <span className="info-text">
+                        {movie.description || "No Description"}
+                    </span>
+                </div>
+                <div className="movie-info">
+                    <span className="info-label">Genre: </span>
+                    <span className="info-text">{movie.genre || "No Genre"}</span>
+                </div>
+                <div className="movie-info">
+                    <span className="info-label">Director: </span>
+                    <span className="info-text">{movie.director || "No Director"}</span>
+                </div>
             </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director}</span>
+            <div className="button-container">
+                <Link to={`/`}>
+                    <button className="back-button">Back</button>
+                </Link>
             </div>
-            <div>
-                <span>Description:</span>
-                <span>{movie.description}</span>
-            </div>
-            <div>
-                <span>Genre:</span>
-                <span>{movie.genre}</span>
-            </div>
-            <Link to={`/`}>
-                <button className="back-button" >Back</button>
-            </Link>
         </div>
-    )
+    );
 }
